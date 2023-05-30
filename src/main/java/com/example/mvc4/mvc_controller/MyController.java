f65374d40780ee14e3bfe17ae0f1837524c9683f
Hemantsingh4774 <hemant.singh_cs.ccv19@gla.ac.in>
2023-05-30 16:22:28 +0530

    delete user functionality

--- src/main/java/com/example/mvc4/mvc_controller/MyController.java
+++ src/main/java/com/example/mvc4/mvc_controller/MyController.java
@@ -4,10 +4,10 @@ import com.example.mvc4.mvcmodel.User;
 import com.example.mvc4.mvcmodel.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @Controller
 public class MyController {
@@ -15,19 +15,37 @@ public class MyController {
     @Autowired
     UserRepo userRepo;
 
-    @GetMapping("/add")
+    @GetMapping("/addpage")
     public String addUser(){
         return "useradd";  // refering to the useradd jsp
     }
 
     @GetMapping("/")
-    public String home(){
+    public String viewData(Model model){
+        ArrayList<User> namereturn = (ArrayList<User>) userRepo.findAll();
+        for(User u:namereturn){
+            System.out.println("Name :"+u.getName());
+        }
+        model.addAttribute("users",namereturn);
+        System.out.println(namereturn);
+
+
         return "index";
     }
 
     @PostMapping("/add")
     public String addUser1(@ModelAttribute("user") User user){
         userRepo.save(user);
-        return "index";
+        return "redirect:/";
     }
+    @GetMapping("/deleteuser/{idx}")
+    public String deleteUser(@PathVariable("idx")long user_id){
+        userRepo.deleteById(user_id);
+        return "redirect:/";
+    }
+//
+//    public String updateUser(){
+//
+//    }
 }
+// id name age address hobby
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,5 +1,11 @@
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <body>
-Hello
+<a href="/addpage">Add user</a>
+<c:forEach items="${users}" var="u">
+<p>${u.name}</p>
+    <a href="/deleteuser/${u.id}" >delete user</a>
+    <a href="/updateuser/${u.id}">update user</a>
+</c:forEach>
 </body>
 </html>
